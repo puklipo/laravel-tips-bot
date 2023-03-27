922c3a7357bf188be6dc28c4cd526e5f01679a78
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-27 12:18:42 +0900

    Create PromptTest.php

---
+++ tests/Feature/PromptTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Chat\Prompt;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PromptTest extends TestCase
+{
+    public function test_make(): void
+    {
+        $p = Prompt::make(system: 'sys', prompt: 'test');
+
+        $this->assertSame('sys', $p->toArray()['messages'][0]['content']);
+    }
+
+    public function test_with(): void
+    {
+        $p = Prompt::make(system: 'sys', prompt: 'test')
+                   ->withModel('model')
+                   ->withMaxTokens(1)
+                   ->withTemperature(0.5);
+
+        $this->assertSame('model', $p->toArray()['model']);
+        $this->assertSame(1, $p->toArray()['max_tokens']);
+        $this->assertSame(0.5, $p->toArray()['temperature']);
+    }
+
+    public function test_closure(): void
+    {
+        $p = new Prompt(
+            system: 'sys',
+            prompt: fn () => 'test',
+        );
+
+        $this->assertSame('test', $p->toArray()['messages'][1]['content']);
+    }
+}
