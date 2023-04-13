60923be2e64995c3ef050c103fd06fae3deaa664
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-13 14:58:09 +0900

    Create ReleaseTest.php

---
+++ tests/Feature/ReleaseTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Notification;
+use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI\Responses\Chat\CreateResponse;
+use Tests\TestCase;
+
+class ReleaseTest extends TestCase
+{
+    public function test_release(): void
+    {
+        Notification::fake();
+
+        Http::fake([
+            '*' => Http::response([
+                [
+                    'published_at' => now()->toDateTimeString(),
+                    'body' => 'test',
+                    'tag_name' => 'v10.0.0',
+                    'html_url' => 'https://',
+                ],
+            ]),
+        ]);
+
+        OpenAI::fake([
+            CreateResponse::fake([
+                'choices' => [
+                    [
+                        'message' => [
+                            'content' => 'test',
+                        ],
+                    ],
+                ],
+            ]),
+        ]);
+
+        $response = $this->artisan('chat:release');
+
+        $response->assertSuccessful();
+    }
+}
