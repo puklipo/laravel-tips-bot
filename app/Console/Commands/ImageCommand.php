7e57d41d05e8dc708e9504040bc36ef00d2ebf3b
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-11-12 12:28:30 +0900

    Create ImageCommand.php

---
+++ app/Console/Commands/ImageCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use OpenAI\Laravel\Facades\OpenAI;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+
+class ImageCommand extends Command implements PromptsForMissingInput
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'image {prompt}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = OpenAI::images()->create([
+            'model' => 'dall-e-3',
+            'prompt' => $this->argument('prompt'),
+            'size' => '1024x1024',
+            'quality' => 'hd',
+            'response_format' => 'url',
+        ]);
+
+        $this->info($response->data[0]['url']);
+    }
+}
