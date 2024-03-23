72c851975f7f05ff2514912288d5c14643911b27
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-23 11:12:09 +0900

    Update HttpChannel.php

--- app/Notifications/Channels/HttpChannel.php
+++ app/Notifications/Channels/HttpChannel.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications\Channels;
 
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Http;
 
@@ -9,6 +10,7 @@ class HttpChannel
 {
     /**
      * Send the given notification.
+     * @throws RequestException
      */
     public function send(object $notifiable, Notification $notification): void
     {
@@ -16,7 +18,8 @@ class HttpChannel
         $token = $notifiable->routeNotificationFor('http', $notification);
 
         $response = Http::withToken($token)
-            ->post('https://puklipo.com/api/status', $message);
+            ->post('https://puklipo.com/api/status', $message)
+            ->throw();
 
         info($response->body());
     }
