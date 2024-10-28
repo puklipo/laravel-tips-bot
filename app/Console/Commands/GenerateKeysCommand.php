5fa29ad2ad6e298ff6400d03301b2719a21d8c54
puklipo <pcs.engineer.team@gmail.com>
2024-10-28 11:28:08 +0900

    Create GenerateKeysCommand.php

---
+++ app/Console/Commands/GenerateKeysCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Revolution\Nostr\Facades\Nostr;
+
+class GenerateKeysCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nostr:generate-keys';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $keys = Nostr::native()->key()->generate();
+
+        //$keys = Nostr::native()->key()->fromNsec(nsec: '');
+
+        $this->info('NOSTR_SK='.$keys->json('sk', ''));
+        $this->line('NOSTR_NSEC='.$keys->json('nsec', ''));
+        $this->line('NOSTR_PK='.$keys->json('pk', ''));
+        $this->line('NOSTR_NPUB='.$keys->json('npub', ''));
+
+        return 0;
+    }
+}
