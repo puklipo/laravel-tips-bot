9008bef4543f29d4c0afd47d5bf126641b878fe5
puklipo <88759954+puklipo@users.noreply.github.com>
2025-12-27 14:47:04 +0900

    Add new Nostr relays to configuration

--- config/nostr.php
+++ config/nostr.php
@@ -24,6 +24,26 @@ return [
     'relays' => [
         // 'wss://relay.nostr.band',
 
+        'wss://x.kojira.io',
+
         'wss://relay.damus.io',
+        'wss://nos.lol',
+        'wss://nostr.oxtr.dev',
+        'wss://relay.nostr.net',
+        'wss://relay.primal.net',
+        'wss://relay.snort.social',
+        'wss://nostr.bitcoiner.social',
+        'wss://nostr-pub.wellorder.net',
+        'wss://nostr.einundzwanzig.space',
+
+        'wss://yabu.me',
+        'wss://relay.barine.co',
+        'wss://nostr-relay.h3z.jp',
+        'wss://nostr.fediverse.jp',
+        // 'wss://relay-jp.shino3.net',
+        'wss://lang.relays.land/ja/',
+        'wss://relay.nostr.wirednet.jp',
+        // 'wss://nostr-relay.nokotaro.com',
+        'wss://relay-jp.nostr.wirednet.jp',
     ],
 ];
