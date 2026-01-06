cfa40483caaa6f5dbd69504e4c55a5149ccfd38b
puklipo <88759954+puklipo@users.noreply.github.com>
2026-01-06 10:32:55 +0900

    pint

--- config/nostr.php
+++ config/nostr.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'keys' => [
         'sk' => env('NOSTR_SK'),
--- config/tips.php
+++ config/tips.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'api_token' => env('TIPS_API_TOKEN'),
 ];
